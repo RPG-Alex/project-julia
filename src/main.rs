4fa8261fa1645013b9d5f7d7a5e4bb5338ea052c
RPG-Alex <mralexblood@outlook.com>
2024-01-03 20:53:55 +0800

    experimenting with how to render fractals

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, julia_fractal_system)
+        //+.add_systems(Update, julia_fractal_system)
         .run();
 }
 
@@ -13,23 +13,7 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn update_fractal(mut texture: ResMut<YourTextureType>, window_size: Res<WindowSize>) {
-    let (width, height) = (window_size.width as usize, window_size.height as usize);
 
-    for x in 0..width {
-        for y in 0..height {
-            // Convert screen coordinates to fractal coordinates
-            let (fractal_x, fractal_y) = screen_to_fractal(x, y, width, height);
-            let color = julia(fractal_x, fractal_y);
-            texture.set_pixel(x, y, color);
-        }
-    }
-}
-
-// System to generate and render Julia fractals
-fn julia_fractal_system() {
-    
-}
 
 fn julia(c_re: f64, c_im: f64, x: f64, y: f64) -> u8 {
     let c = Complex::new(c_re, c_im);
