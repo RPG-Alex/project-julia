148d6327b2cf4147c0eda008f5136b490ecd99f7
Silzinc <jonasbloch85@gmail.com>
2024-06-09 23:16:25 +0200

    Added resource for mouse screen position, improved screen dragging

--- src/controll.rs
+++ src/controll.rs
@@ -1,16 +1,23 @@
 use bevy::{
   ecs::{
     event::EventReader,
-    system::{Query, ResMut},
+    system::{Query, Res, ResMut, Resource},
   },
   input::{
-    mouse::{MouseButton, MouseMotion, MouseWheel},
+    mouse::{MouseButton, MouseWheel},
     ButtonInput,
   },
-  window::{CursorIcon, Window},
+  math::{Vec2, Vec4},
+  window::{CursorIcon, CursorMoved, Window},
 };
 
 use crate::sets::julia::PostProcessSettings;
+
+#[derive(Resource)]
+pub struct MouseState
+{
+  pub position: Vec2,
+}
 
 pub fn zoom_with_mouse_wheel(
   mut scroll_events: EventReader<MouseWheel>,
@@ -19,43 +26,53 @@ pub fn zoom_with_mouse_wheel(
 {
   for mut settings in settings.iter_mut() {
     for event in scroll_events.read() {
-      match event.y {
-        // Positive y value means scrolling up (zoom in)
-        // Negative y value means scrolling down (zoom out)
-        _ if event.y > 0.0 => {
-          settings.view.w *= 0.9;
-          settings.view.z *= 0.9;
-          settings.max_iter += 3;
-        }, // Zoom in
-        _ => {
-          settings.view.w *= 1.1;
-          settings.view.z *= 1.1;
-          settings.max_iter -= 3;
-          settings.max_iter = settings.max_iter.max(100);
-        }, // Zoom out
-      }
+      if event.y > 0.0 {
+        settings.view.w *= 0.9;
+        settings.view.z *= 0.9;
+        settings.max_iter += 3;
+      } else {
+        settings.view.w *= 1.1;
+        settings.view.z *= 1.1;
+        settings.max_iter -= 3;
+        settings.max_iter = settings.max_iter.max(100);
+      } // Zoom out
     }
   }
 }
 
+fn screen_position_to_complex(position: Vec2, screen: Vec2, view: Vec4) -> Vec2
+{
+  Vec2::new(
+    (position.x - screen.x / 2.0) / screen.x * view.z + view.x,
+    (screen.y / 2.0 - position.y) / screen.y * view.w + view.y,
+  )
+}
+
 pub fn click_and_drag_with_mouse(
-  mut mouse_event: EventReader<MouseMotion>,
+  mut mouse_event: EventReader<CursorMoved>,
   mut settings: Query<&mut PostProcessSettings>,
   mut windows: Query<&mut Window>,
-  mouse_click: ResMut<ButtonInput<MouseButton>>,
+  mouse_click: Res<ButtonInput<MouseButton>>,
+  mut mouse_state: ResMut<MouseState>,
 )
 {
   if let Some(mut window) = windows.iter_mut().next() {
-    if mouse_click.pressed(MouseButton::Left) {
-      window.cursor.icon = CursorIcon::Move;
-      for event in mouse_event.read() {
+    for event in mouse_event.read() {
+      if mouse_click.pressed(MouseButton::Left) {
+        window.cursor.icon = CursorIcon::Move;
         for mut setting in settings.iter_mut() {
-          setting.view.x += -event.delta.x * 0.003 * setting.view.z;
-          setting.view.y += event.delta.y * 0.003 * setting.view.z;
+          let complex_target =
+            screen_position_to_complex(mouse_state.position, setting.screen, setting.view);
+          let current_mouse_complex =
+            screen_position_to_complex(event.position, setting.screen, setting.view);
+          let complex_shift = complex_target - current_mouse_complex;
+          setting.view.x += complex_shift.x;
+          setting.view.y += complex_shift.y;
         }
+      } else {
+        window.cursor.icon = CursorIcon::Default;
       }
-    } else {
-      window.cursor.icon = CursorIcon::Default;
+      mouse_state.position = event.position;
     }
   }
 }
